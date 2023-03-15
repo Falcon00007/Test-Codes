53fefab29ed886714feb951cb3c57644a2ac058a
Falcon00007 <yofalcon98@gmail.com>
2023-03-15 20:37:28 +0530

    DOM manipulaton Task-10

---
+++ DOM Task-10 (Local Storage)/dom10.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('#my-form');
+const userName = document.getElementById("name");
+const email = document.getElementById("email");
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+form.addEventListener('submit', onsubmit);
+
+function onsubmit(e) {
+    e.preventDefault();
+    if (userName.value === "" || email.value === "") {
+        msg.classList.add("error");
+        msg.innerHTML = "Pleaser enter all fields";
+        setTimeout(() => msg.remove(), 3000);
+    }
+    else {
+        var li = document.createElement("li");
+        var input = document.createTextNode(`${userName.value} : ${email.value}`);
+        li.appendChild(input);
+        userList.appendChild(li);
+
+        localStorage.setItem("name", userName.value);
+        console.log(localStorage.getItem("name"));
+        localStorage.setItem("email", email.value);
+        console.log(localStorage.getItem("email"));
+
+        userName.value = "";
+        email.value = "";
+
+    }
+}
+
